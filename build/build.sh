8e1d7ac0f76f5bbcb44de440d1a8671bab2217b6
linclaus <linclaus@outlook.com>
2021-01-31 21:54:37 +0800

    build

--- build/build.sh
+++ build/build.sh
@@ -4,3 +4,4 @@ set -x
 CI_COMMIT_TAG=$(git describe --always --tags)
 
 docker build -t linclaus/prometheus-operator:latest -f build/package/Dockerfile .
+docker push linclaus/prometheus-operator:latest
